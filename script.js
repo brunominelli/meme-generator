6fb1b86f8325a2d7f642dbcd9660829ffcd193b3
Bruno Minelli <minellibruno@hotmail.com>
2021-12-15 16:30:05 -0300

    Requisitos 01, 04, 05 e 06 concluídos.

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-const elementInput = document.getElementById('text-input');
-const elementeParagraph = document.getElementById('meme-text');
+// Entrada de Texto
+const elementInputText = document.getElementById('text-input');
+const elementeMemeText = document.getElementById('meme-text');
 
-elementInput.addEventListener('input', () => {
-  elementeParagraph.innerText = elementInput.value;
-});
+// Imagem do Meme
+const elementInputFile = document.getElementById('meme-insert');
+const elementeMemeImage = document.getElementById('meme-image');
+
+// Container
+const elementContainer = document.getElementById('meme-image-container');
+
+// Botões
+const buttonFire = document.getElementById('fire');
+const buttonWater = document.getElementById('water');
+const buttonEarth = document.getElementById('earth');
+
+function insertMemeText() {
+  const text = elementInputText.value;
+  elementeMemeText.innerText = text;
+}
+
+function insertMemeImage(event) {
+  elementeMemeImage.src = URL.createObjectURL(event.target.files[0]);
+}
+
+function setBorderFire() {
+  elementContainer.style.border = '3px dashed red';
+}
+
+function setBorderWater() {
+  elementContainer.style.border = '5px double blue';
+}
+
+function setBorderEarth() {
+  elementContainer.style.border = '6px groove green';
+}
+
+elementInputFile.addEventListener('change', insertMemeImage);
+elementInputText.addEventListener('input', insertMemeText);
+buttonFire.addEventListener('click', setBorderFire);
+buttonWater.addEventListener('click', setBorderWater);
+buttonEarth.addEventListener('click', setBorderEarth);
